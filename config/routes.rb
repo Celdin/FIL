485e544edb679a95a34c6c55c44206568a8896ed
Celdin <Sylvain.goulliart@gmail.com>
2015-07-27 23:55:08 +0200

    Set des Routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-root 'projetq#index'
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
@@ -53,4 +53,26 @@ root 'projetq#index'
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+	root 'projet#index'
+
+	# Projet
+	get 'projet/'            => 'projet#index'
+	get 'projet/:id'         => 'projet#show', as: 'projet'
+	get 'projet/new'         => 'projet#new'
+	get 'projet/:id/update'  => 'projet#update'
+	get 'projet/:id/delete'  => 'projet#delete'
+
+	#User
+	get 'user/'              => 'user#index'
+	get 'user/:id'           => 'user#show', as: 'user'
+	get 'user/new'           => 'user#new'
+	get 'user/:id/update'    => 'user#update'
+	get 'user/:id/delete'    => 'user#delete'
+
+	#Rendu
+	get 'rendu/'             => 'rendu#index'
+	get 'rendu/:id'          => 'rendu#show', as: 'rendu'
+	get 'rendu/new'          => 'rendu#new'
+	get 'rendu/:id/update'   => 'rendu#update'
+	get 'rendu/:id/delete'   => 'rendu#delete'
 end

9a54ad451af19e64cf13d64136b9bb25a9256639
Celdin <Sylvain.goulliart@gmail.com>
2015-07-28 23:09:22 +0200

    Genetation du scaffold "User"

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,2 @@
 class User < ActiveRecord::Base
-	validate :mail, format: with: /regex/i
-	has_many :projets
-	has_many :rendus
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :users
+
   resources :projets
 
   # The priority is based upon order of creation: first created -> highest priority.
